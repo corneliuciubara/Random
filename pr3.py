23eefe57c2966e4bafcd90427a05cdac9d1724da
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-10-04 10:28:21 +0300

    Update pr3.py

--- pr3.py
+++ pr3.py
@@ -1,1 +1,7 @@
-
+import random
+n = random.randint(100,999)
+print("n =",n)
+if n>0 and n%6==0 and n%8==0:
+    print("Numarul n este divizibil cu 6 si cu 8")
+else:
+    print("Numarul n nu este divizibil cu 6 si cu 8")
