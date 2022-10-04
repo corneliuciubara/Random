479240e5ea739e4c58f420913494330de6dbe2ba
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-10-04 10:12:39 +0300

    Update pr1.py

--- pr1.py
+++ pr1.py
@@ -1,1 +1,9 @@
-
+import random
+a = random.randint(0,100)
+b = random.randint(0,100)
+print("a=",a)
+print("b=",b)
+print("a) a + b =", a + b)
+print("b) a - b =", a - b)
+print("b) b - a =", b - a)
+print("c) a * b =", a * b)
