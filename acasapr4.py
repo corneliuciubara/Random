a2df01b5bd0982380276f3c19e8f6449ee49c276
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-10-10 15:06:54 +0300

    Update acasapr4.py

--- acasapr4.py
+++ acasapr4.py
@@ -3,86 +3,259 @@ nume = str(input("Care este numele tău?: "))
 print("Salut, ", nume)
 import random
 print("Dimeniul matematic:")
+m = 0
+t = 0
 rs1 = input("Cu cât este egal 2 + 2: ")
 if rs1 == "4":
+    t+=1
+    m+=1
     print("Corect!")
 else:
     print("Greșit!")
 rs2 = input("4 la puterea 2 este egal cu: ")
 if rs2 == "16":
+    t+=1
+    m+=1
     print("Corect!")
 else:
     print("Greșit!")
 rs3 = input("a + a + 3a = ")
 if rs3 == "5a":
+    t+=1
+    m+=1
     print("Corect!")
 else:
     print("Greșit!")
 rs4 = input("100*100 = ")
 if rs4 == "10000":
+    t+=1
+    m+=1
     print("Corect!")
 else:
     print("Greșit!")
 rs5 = input("67-7+3 = ")
 if rs5 == "63":
+    t+=1
+    m+=1
     print("Corect!")
 else:
     print("Greșit!")
 
 rs6 = input("10 la puterea 9 este = ")
 if rs6 == "1000000000":
+    t+=1
+    m+=1
     print("Corect!")
 else:
     print("Greșit!")
 
 rs7 = input("5 + 1000000000 = ")
 if rs7 == "1000000005":
+    t+=1
+    m+=1
     print("Corect!")
 else:
     print("Greșit!")
 
 rs8 = input("sinus de 90° = ")
 if rs8 == "1":
+    t+=1
+    m+=1
     print("Corect!")
 else:
     print("Greșit!")
 
 rs9 = input("50*10=")
 if rs9 == "500":
+    t+=1
+    m+=1
     print("Corect!")
 else:
     print("Greșit!")
 
 rs10 = input("5% din 200 este = ")
 if rs10 == "10":
+    t+=1
+    m+=1
     print("Corect!")
 else:
     print("Greșit")
 
-print("Domeniul informatic:")
+print("Nota la matematică:", m)
+
+
+print("Domeniul informatic: Python")
+i = 0
 ir1 = input("Caracterul care este necesar de a scri un comenatiu este: ")
 if ir1=="#":
+    t+=1
+    i+=1
     print("Corect!")
 else:
     print("Greșit!")
 
 ir2 = input("Extensia corectă a unui fișier python este: ")
 if ir2==".py":
+    t+=1
+    i+=1
     print("Corect!")
 else:
     print("Greșit!")
 
 ir3 = input("Scrie sintaxa corectă care afișeaza tipul variabilei x:")
 if ir3=="print(type(x))":
+    t+=1
+    i+=1
     print("Corect!")
 else:
     print("Greșit!")
 
 ir4 = input("Creaza o functie cu denumirea func, care nu are parametri: ")
 if ir4=="def func():":
+    t+=1
+    i+=1
     print("Corect!")
 else:
     print("Greșit!")
+
+ir5 = input("Operatorul care determina lungimea unei liste este:")
+if ir5=="len()":
+    t+=1
+    i+=1
+    print("Corect!")
+else:
+    print("Greșit!")
+
+ir6 = input("Operatorul care transforma toate literele mici dintr-un string in litere mari")
+if ir6=="upper()":
+    t+=1
+    i+=1
+    print("Corect!")
+else:
+    print("Greșit!")
+
+ir7 = input("Operatorul care înlocuiește părți dintr-un string:")
+if ir7=="replace()":
+    t+=1
+    i+=1
+    print("Corect!")
+else:
+    print("Greșit!")
+
+ir8 = input("Operatorul care returneaza restul împărtirii unui nr la altul: ")
+if ir8=="%":
+    t+=1
+    i+=1
+    print("Corect!")
+else:
+    print("Greșit!")
+
+ir9 = input("Care colecție e aranjată, schimbabilă, și poate multiplica membri? ")
+if ir9=="list":
+    t+=1
+    i+=1
+    print("Corect!")
+else:
+    print("Greșit!")
+
+ir5 = input("Functia care oprește un ciclu: ")
+if ir5=="break":
+    t+=1
+    i+=1
+    print("Corect!")
+else:
+    print("Greșit!")
+
+print("Nota la informatică(python): ", i)
+
+print("Domeniul fizicii:")
+fi1 = input("Cu ce simbol se notaza căldura: ")
+f = 0
+if fi1=="Q":
+    t+=1
+    f+=1
+    print("Corect!")
+else:
+    print("Greșit!")
+
+
+fi2 = input("Cu ce simbol se noteaza rezistenți
+a totală: ")
+if fi2=="R":
+    t+=1
+    f+=1
+    print("Corect!")
+else:
+    print("Greșit!")
+
+fi3 = input("Cu ce simbol se noteaza impulsul: ")
+if fi3=="p":
+    t+=1
+    f+=1
+    print("Corect!")
+else:
+    print("Greșit!")
+
+fi4 = input("Cu ce simbol se notează intensitatea curentuluii:")
+if fi4=="U":
+    t+=1
+    f+=1
+    print("Corect!")
+else:
+    print("Greșit!")
+
+fi5 = input("Cu ce simbol se notează t.e.m.")
+if fi5=="E":
+    t+=1
+    f+=1
+    print("Corect!")
+else:
+    print("Greșit!")
+
+fi6 = input("Cu ce simbol se noteaza concentrația: ")
+if fi6=="n":
+    t+=1
+    f+=1
+    print("Corect!")
+else:
+    print("Greșit!")
+
+fi7 = input("Cu ce simbol se notează ")
+if fi7=="":
+    t+=1
+    f+=1
+    print("Corect!")
+else:
+    print("Greșit!")
+
+
+fi8 = input("")
+if fi8=="":
+    t+=1
+    f+=1
+    print("Corect!")
+else:
+    print("Greșit!")
+
+fi9 = input("")
+if fi9=="":
+    t+=1
+    f+=1
+    print("Corect!")
+else:
+    print("Greșit!")
+
+
+fi10 = input("")
+if fi10=="":
+    t+=1
+    f+=1
+    print("Corect!")
+else:
+    print("Greșit!")
+
+
+
 
 
 
