9e2e33a10ded1ed732b82f2523dc5e14b8d393d1
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-10-10 10:42:17 +0300

    Update 05.10.22pr2.py

--- 05.10.22pr2.py
+++ 05.10.22pr2.py
@@ -2,9 +2,9 @@ import random
 n = int(input("Numarul de aruncari = "))
 s = 0 
 for i in range(n):
-    i = random.randint(1, 6)
-    print(i)
+    y = random.randint(1, 6)
+    print(y)
     if i == 6:
         s += 1
-print("Nr 6 a fost de ", s, "ori")
+print("Numarul 6 a aparut de ", s, "ori")
 
