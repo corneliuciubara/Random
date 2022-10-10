ee78c0a707bfd08a588d6618f87a3655f55db43a
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-10-10 10:56:50 +0300

    Update 05.10.22pr3.py

--- 05.10.22pr3.py
+++ 05.10.22pr3.py
@@ -1,18 +1,21 @@
-import random 
-n = random.randint(1, 999999999)
-print("n =", n)
-d = int(input("Introduce cifra: "))
-s = 0
-def noc():
-    global n 
-    global d
-    count = 0
-    while (n > 0):  
-        if(n % 10 == d):
-            count = count + 1
-        n = n // 10
-    return count
-if d>0 and d<10:
-    print("Numarul de aparitii al lui", d, "este:", noc())
-else:
-    print("Cifra", d, "nu este inte 1 si 9")
+from random import randint
+k = []
+for i in range(1):
+    x = randint(0,999999999)
+    print(x)
+while x>0:
+    r = x%10
+    x = // 10
+    k.append(r)
+print("0 apare de:", k.count(0), "ori")
+print("1 apare de:", k.count(1), "ori")
+print("2 apare de:", k.count(2), "ori")
+print("3 apare de:", k.count(3), "ori")
+print("4 apare de:", k.count(4), "ori")
+print("5 apare de:", k.count(5), "ori")
+print("6 apare de:", k.count(6), "ori")
+print("7 apare de:", k.count(7), "ori")
+print("8 apare de:", k.count(8), "ori")
+print("9 apare de:", k.count(9), "ori")
+
+
