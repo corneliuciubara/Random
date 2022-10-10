d6d6e4f40310f99ee60b089efb52f4c1a6cd0519
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-10-10 10:39:18 +0300

    Update 05.10.22pr1.py

--- 05.10.22pr1.py
+++ 05.10.22pr1.py
@@ -1,13 +1,13 @@
 import random 
 n = int(input("Numarul de cifre = "))
-sp = 0
-sn = 0
+spoz = 0
+sneg = 0
 for i in range(n):
     i = random.randint(-50, 50)
     print(i)
-    if i < 0:
-        sp += i
     if i > 0:
-        sn += i
-print("Spoz =", sn)
-print("Sneg =",sp)
+        spoz += i
+    if i < 0:
+        sneg += i
+print("Spoz =", spoz)
+print("Sneg =",sneg)
