4527c41d4ead5466faf4b354ea009927bd62ccc9
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-10-10 10:59:51 +0300

    Update 05.10.22pr4.py

--- 05.10.22pr4.py
+++ 05.10.22pr4.py
@@ -4,7 +4,7 @@ z2 = random.randint(1,6)
 print("Zarul 1 are fata:", z1)
 print("Zarul 2 are fata:", z2)
 if z1 == z2:
-    print(z1+z2)
+    print("Suma este:", z1+z2)
 else:
     print("Zarurile nu au fete comune")
 
