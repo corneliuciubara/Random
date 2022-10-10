f0032f3f214409c964bd4187ba4c3b8f7285dfec
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-10-10 12:27:55 +0300

    Update acasapr4.py

--- acasapr4.py
+++ acasapr4.py
@@ -1,3 +1,14 @@
 print("Vreau să fiu milionar")
+nume = str(input("Care este numele tău?: "))
+print("Salut, {nume}")
 import random
-tema = ["Matematica", "Informatica", "Limba Română", "Chimie", "Biologie"]
+print("Dimeniul matematic:")
+rs1 = input("Cu cât este egal 2 + 2: ")
+if rs1 == "4":
+    print("Corect!")
+else:
+    print("Greșit!")
+rs2 = input("4 la puterea 2 este egal cu: ")
+
+
+
