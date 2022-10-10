e9db6ece6f4295326a9104252ae408e465c8c05b
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-10-10 21:38:10 +0300

    Update acasapr3.py

--- acasapr3.py
+++ acasapr3.py
@@ -1,7 +1,3 @@
-import random
-n = random.randint(100,999)
-print("n =",n)
-if n>0 and n%6==0 and n%8==0:
-    print("Numarul n este divizibil cu 6 si cu 8")
-else:
-    print("Numarul n nu este divizibil cu 6 si cu 8")
+from random import randint
+numar = 48*randint(3, 19)
+print(numar)
