6c6ea2665057f743017228e982f0b061ed2aa7c1
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-10-10 13:36:32 +0300

    Update acasapr4.py

--- acasapr4.py
+++ acasapr4.py
@@ -1,6 +1,6 @@
 print("Vreau să fiu milionar")
 nume = str(input("Care este numele tău?: "))
-print("Salut, {nume}")
+print("Salut, ", nume)
 import random
 print("Dimeniul matematic:")
 rs1 = input("Cu cât este egal 2 + 2: ")
@@ -12,46 +12,46 @@ rs2 = input("4 la puterea 2 este egal cu: ")
 if rs2 == "16":
     print("Corect!")
 else:
-    print("Greșit")
+    print("Greșit!")
 rs3 = input("a + a + 3a = ")
 if rs3 == "5a":
     print("Corect!")
 else:
-    print("Greșit")
+    print("Greșit!")
 rs4 = input("100*100 = ")
 if rs4 == "10000":
     print("Corect!")
 else:
-    print("Greșit")
+    print("Greșit!")
 rs5 = input("67-7+3 = ")
 if rs5 == "63":
     print("Corect!")
 else:
-    print("Greșit")
+    print("Greșit!")
 
 rs6 = input("10 la puterea 9 este = ")
 if rs6 == "1000000000":
     print("Corect!")
 else:
-    print("Greșit")
+    print("Greșit!")
 
 rs7 = input("5 + 1000000000 = ")
 if rs7 == "1000000005":
     print("Corect!")
 else:
-    print("Greșit")
+    print("Greșit!")
 
 rs8 = input("sinus de 90° = ")
 if rs8 == "1":
     print("Corect!")
 else:
-    print("Greșit")
+    print("Greșit!")
 
 rs9 = input("50*10=")
 if rs9 == "500":
     print("Corect!")
 else:
-    print("Greșit")
+    print("Greșit!")
 
 rs10 = input("5% din 200 este = ")
 if rs10 == "10":
@@ -60,11 +60,37 @@ else:
     print("Greșit")
 
 print("Domeniul informatic:")
-ir1 = input("")
-if ir1=="":
+ir1 = input("Caracterul care este necesar de a scri un comenatiu este: ")
+if ir1=="#":
     print("Corect!")
 else:
     print("Greșit!")
+
+ir2 = input("Extensia corectă a unui fișier python este: ")
+if ir2==".py":
+    print("Corect!")
+else:
+    print("Greșit!")
+
+ir3 = input("Scrie sintaxa corectă care afișeaza tipul variabilei x:")
+if ir3=="print(type(x))":
+    print("Corect!")
+else:
+    print("Greșit!")
+
+ir4 = input("Creaza o functie cu denumirea func, care nu are parametri: ")
+if ir4=="def func():":
+    print("Corect!")
+else:
+    print("Greșit!")
+
+
+
+
+
+
+
+
 
 
 
